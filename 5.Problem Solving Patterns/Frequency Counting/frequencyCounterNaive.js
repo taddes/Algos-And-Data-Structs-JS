5af35064ba5465a4512c694273eec4c43c0c1e84
Taddes Korris <taddes.korris@gmail.com>
2020-05-22 16:44:51 -0400

    naive and O(n) approaches using objects to create a frequency count map

--- 5.Problem Solving Patterns/Frequency Counting/frequencyCounterNaive.js
+++ 5.Problem Solving Patterns/Frequency Counting/frequencyCounterNaive.js
@@ -7,10 +7,21 @@ same([1,2,3], [4,1,9]) // True
 same([1,2,3], [1,9]) // false
 same([1,2,1], [4,4,1]) // true
 
+O(n2) solution - Quadratic relationship
+if 1000 values in outer loop, 1000 in inner loop, results in 1000 * 1000 = 1000000!!!
+
 */
 
-function freqCount(arr1, arr2) {
+function same_naive(arr1, arr2) {
   if (arr1.length !== arr2.length) {
-    return;
+    return false;
   }
+  for (let i = 0; i < arr1.length; i++) {
+    let correctIndex = arr2.indexOf(arr[i] ** 2)
+    if (correctIndex === -1) {
+      return false;
+    }
+    arr.splice(correctIndex, 1)
+  }
+  return true
 }
