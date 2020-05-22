0a77ee39bffbd53a218d036b97b2cc04dd087d7c
Taddes Korris <taddes.korris@gmail.com>
2020-05-22 17:14:46 -0400

    sum zero

---
+++ 5.Problem Solving Patterns/Multiple Pointers/sumZero.js
@@ -0,0 +1,6 @@
+/*
+Write a function, sumZero which accepts a sorted array of integers.
+The function should find the first pair where the sum is 0. Return
+an array that includes both values of that sum to zero, or undefined
+if a pair does not exist.  
+*/
