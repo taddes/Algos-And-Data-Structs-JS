0a1f5a71cdecc4eef5ec0f7237578bbdb09964bf
Taddes Korris <taddes.korris@gmail.com>
2020-06-25 14:09:28 -0400

    more bubble sort

--- 11.Bubble Sort/bubbleSort.js
+++ 11.Bubble Sort/bubbleSort.js
@@ -22,7 +22,8 @@ function bubbleSortOptimized(arr) {
   for (let i = arr.length; i > 0; i--) {
     noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
-      if (arr[j] > arr[j+1] ) {
+      console.log(arr, arr[j], arr[j+1]);
+      if (arr[j] > arr[j+1]) {
         let temp = arr[j];
         arr[j] = arr[j+1];
         arr[j+1] = temp;
