ca0cebe06b94fd19fb06b474108510f9a9c07a0d
Taddes Korris <taddes.korris@gmail.com>
2020-06-24 22:52:46 -0400

    more bubble sort

--- 11.Bubble Sort/blank_bubbleSort.js
+++ 11.Bubble Sort/blank_bubbleSort.js
@@ -12,3 +12,20 @@ function bubble(arr) {
 }
 
 console.log(bubble([45, 8, 16, 32, 2]))
+
+function bubbleBest(arr) {
+ for (let i = arr.length; i > 0; i--) {
+   for (let j = 0; j < i - 1; j++) {
+     if (arr[j] > arr[j+1]) {
+       let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp
+     }
+
+   }
+ }
+  return arr;
+}
+
+
+console.log(bubbleBest([45, 8, 16, 32, 2]))
