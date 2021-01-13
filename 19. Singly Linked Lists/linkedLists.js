d4b5bfc9f387e3269a5ccb2e28b84167a8709259
Taddes Korris <taddes.korris@gmail.com>
2021-01-13 18:20:40 -0500

    get, insert, and set

--- 19. Singly Linked Lists/linkedLists.js
+++ 19. Singly Linked Lists/linkedLists.js
@@ -16,7 +16,7 @@ class SinglyLinkedList {
     let newNode = new Node(val);
     if (this.length === 0 & !this.head) {
       this.head = newNode;
-      this.tail = this.head;
+      this.tail = newNode;
     } else {
       this.tail.next = newNode;
       this.tail = newNode;
@@ -59,16 +59,74 @@ class SinglyLinkedList {
     return currentHead;
   }
 
+  unshift(val) {
+    // Add new element to start of list
+    // Insert new head, point old head to next prop of new head
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(idx) {
+    if (this.length === 0 || idx < 0 || idx >= this.length) return null;
+    let currentIndex = 0;
+    let currentNode = this.head;
+    while (currentIndex <= idx) {
+      currentIndex++;
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  }
+
+  set(index, value) {
+    // Update value at a particular index
+    let foundNode = this.get(index);
+    if (foundNode){
+      foundNode.val = value;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, value) {
+    // add a node at a specific position
+    if (index > this.length || index < 0) return false;
+    if (index === this.length) return this.push(value);
+    if (index === 0) return this.unshift(value)
+
+    let oldNodeAtLocation = this.get(index);
+    let priorNode = this.get(index-1)
+    let newNode = new Node(value);
+    newNode.next = oldNodeAtLocation;
+    priorNode.next = newNode;
+    this.length++;
+    return true
+  }
+
 }
 
 list = new SinglyLinkedList()
 list.push('hey')
 list.push('yo')
 list.push('hello')
-list.push('ey')
-list.push('yooo')
+// list.push('ey')
+// list.push('yooo')
+// console.log(list)
+// list.shift()
+// console.log(list)
+// list.shift()
+// console.log(list)
+// list.unshift('whut')
+// console.log(list)
+console.log(list.get(2))
+list.insert(1, 'test')
 console.log(list)
-list.shift()
-console.log(list)
-list.shift()
-console.log(list)
+console.log(list.get(1))
