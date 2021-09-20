8a6917c3fbb49826923133eb05c2e8fbb29f97d0
Taddes Korris <taddes.korris@passportinc.com>
2021-09-20 15:29:30 -0400

    binary srch

--- 10.Searching Algorithms/binarySearch.js
+++ 10.Searching Algorithms/binarySearch.js
@@ -1,19 +1,22 @@
 function binarySearch(arr, item) {
-  let left = 0;
-  let right = arr.length - 1;
-  let midpoint = Math.floor((arr.length -1) / 2)
+  let start = 0;
+  let end = arr.length - 1;
+  let midpoint = Math.floor((arr.length -1) / 2);
 
-  while (left > right) {
-    if (item === arr[midpoint]) {
-      return arr[item]
-    } else if (item > arr[midpoint]) {
-      left = midpoint
-      right =
-    } else if (item < arr[midpoint]) {
-
+  while (arr[midpoint] !== item && start <= end) {
+    console.log(start, middle, end);
+    if (item < arr[midpoint]) {
+      end = middle -1;
+    } else {
+      start = middle + 1
     }
-
-    
-
+    midpoint = Math.floor((arr.length -1) / 2);
   }
+  console.log(start, middle, end);
+  if (arr[midpoint] == item) {
+    return middle;
+  }
+  return -1;
 }
+
+console.log(binarySearch([1,2,3,4,5,6,7,8,9,10], 5))
